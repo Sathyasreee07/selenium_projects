a9d64af032680e077e6c7a52364333bc3ae2522b
Sathyasreee07 <131867618+Sathyasreee07@users.noreply.github.com>
2026-03-27 14:03:58 +0530

    Add files via upload

---
+++ HandlingSynchronization/src/test/java/selenium_wait/ImplicitWait.java
@@ -0,0 +1,22 @@
+package selenium_wait;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+	}
+
+}
